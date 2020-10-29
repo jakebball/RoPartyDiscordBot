2f2d2952799743bb4d5fa067b3c06ef26d0440a8
jake <jakebball2014@gmail.com>
2020-10-28 20:12:18 -0700

    added support for canceling

--- src/main/java/com/discord/main/Main.java
+++ src/main/java/com/discord/main/Main.java
@@ -18,6 +18,8 @@ public class Main extends ListenerAdapter {
     private static Config config = new Config();
     private static JDA jda;
     private static int BugReports = 1;
+    private static int ExploitReports = 1;
+    private static ArrayList<User> alreadyFilling = new ArrayList<>();
 
     public static void main(String[] args) throws LoginException, InterruptedException{
 
@@ -40,10 +42,14 @@ public class Main extends ListenerAdapter {
                 event.getMessage().getChannel().sendMessage("<@" + event.getAuthor().getIdLong() + ">, Party! \uD83C\uDF89").complete();
                 break;
             case "bugreport":
-                event.getMessage().getChannel().sendMessage("<@" + event.getAuthor().getIdLong() + ">, bug report will continue in dms").complete();
-                BugReport bugReport = new BugReport(event.getAuthor(), jda, event.getGuild());
-                Thread thread = new Thread(bugReport);
-                thread.start();
+                System.out.println(alreadyFilling.toString());
+                if(!alreadyFilling.contains(event.getAuthor())){
+                    event.getMessage().getChannel().sendMessage("<@" + event.getAuthor().getIdLong() + ">, bug report will continue in dms").complete();
+                    BugReport bugReport = new BugReport(event.getAuthor(), jda, event.getGuild());
+                    Thread bugThread = new Thread(bugReport);
+                    bugThread.start();
+                    alreadyFilling.add(event.getAuthor());
+                }
                 break;
             case "studio":
                 event.getMessage().getChannel().sendMessage("<@" + event.getAuthor().getIdLong() + ">, https://www.roblox.com/groups/4946462/Surfer-Studios#!/about").complete();
@@ -59,6 +65,9 @@ public class Main extends ListenerAdapter {
                 break;
             case "exploitreport":
                 event.getMessage().getChannel().sendMessage("<@" + event.getAuthor().getIdLong() + ">, exploit report will continue in dms").complete();
+                ExploitReport exploitReport = new ExploitReport(event.getAuthor(), jda, event.getGuild());
+                Thread exploitThread = new Thread(exploitReport);
+                exploitThread.start();
                 break;
             case "rohelp":
                 EmbedBuilder embedBuilder = new EmbedBuilder();
@@ -86,26 +95,30 @@ public class Main extends ListenerAdapter {
         private final int timeoutLimit = 120;
         private boolean hasResponded = false;
         private boolean hasFinished = false;
+        private PrivateChannel channel;
 
         public BugReport(User user, JDA jda, Guild guild) {
             jda.addEventListener(this);
             this.user = user;
             this.guild = guild;
+            this.channel = user.openPrivateChannel().complete();
+
             questionsArray.add("Hi! \uD83D\uDC4B Thanks for filling a bug report. Please remember to make sure there is not already a report made in the #bug-reports channel. "
                     + "If you found one, tap the thumbs up so we know you also experienced it. If it has not been made before please answer the questions so we can help squash this bug. "
             + "You will have 3 minutes to answer each question or you can type cancel. Please state your roblox username.");
             questionsArray.add("please state your platform your experienced the bug on. PC, Mac, Xbox, Mobile, Etc.");
             questionsArray.add("please describe your bug in the best detail possible and what caused this to happen");
+
             askQuestion();
         }
 
         public boolean askQuestion(){
            if(currentIndex <= questionsArray.size() - 1){
-               user.openPrivateChannel().complete().sendMessage(questionsArray.get(currentIndex)).queue();
+               channel.sendMessage(questionsArray.get(currentIndex)).queue();
                currentIndex += 1;
                return false;
            } else {
-               user.openPrivateChannel().complete().sendMessage("Thanks for reporting the bug! You view your report in the the #bug-reports channel.").queue();
+               channel.sendMessage("Thanks for reporting the bug! You view your report in the the #bug-reports channel.").queue();
                createReport();
                jda.removeEventListener(this);
                Main.BugReports += 1;
@@ -117,9 +130,14 @@ public class Main extends ListenerAdapter {
         @Override
         public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
             if(event.getAuthor().equals(user)){
-                questionAnswers.add(event.getMessage().getContentRaw());
-                askQuestion();
-                hasResponded = true;
+                if(event.getMessage().getContentRaw().equalsIgnoreCase("cancel")){
+                    hasFinished = true;
+                    channel.sendMessage("Canceled Report").queue();
+                } else {
+                    questionAnswers.add(event.getMessage().getContentRaw());
+                    askQuestion();
+                    hasResponded = true;
+                }
             }
         }
 
@@ -134,6 +152,98 @@ public class Main extends ListenerAdapter {
             if(embedBuilder.isValidLength()){
                 guild.getTextChannelById("769408576485458010").sendMessage("<@" +user.getIdLong() + ">").queue();
                 guild.getTextChannelById("769408576485458010").sendMessage(embedBuilder.build()).queue(message -> message.addReaction("👍").queue());
+            }
+        }
+
+        @Override
+        public void run(){
+
+            Thread thread = Thread.currentThread();
+
+            for(int i=0;i<=timeoutLimit;i++){
+                try {
+                    thread.sleep(1000);
+                } catch (Exception e) {}
+
+                if(hasResponded){
+                    i = 0;
+                    hasResponded = false;
+                }
+
+                if(hasFinished) {
+                    channel.close();
+                    break;
+                }
+            }
+
+            if(!hasFinished){
+                jda.removeEventListener(this);
+                channel.sendMessage("Report Timed Out").queue();
+                channel.close();
+            }
+
+            alreadyFilling.remove(user);
+        }
+    }
+
+    class ExploitReport extends ListenerAdapter implements Runnable{
+        private final User user;
+        private final Guild guild;
+        private ArrayList<String> questionsArray = new ArrayList<>();
+        private ArrayList<String> questionAnswers = new ArrayList<>();
+        private int currentIndex = 0;
+        private final int timeoutLimit = 120;
+        private boolean hasResponded = false;
+        private boolean hasFinished = false;
+
+        public ExploitReport(User user, JDA jda, Guild guild) {
+            jda.addEventListener(this);
+            this.user = user;
+            this.guild = guild;
+            questionsArray.add("Hi! \uD83D\uDC4B Thanks for filling an exploit report. Please answer the questions truthfully and to the best of your ability. " +
+                    "We take your exploit reports very seriously so if you are found to be lying you will receive a kick from the server. If you are kicked more then 2 times you will be permanently banned."
+                    + "You will have 3 minutes to answer each question or you can type cancel. Please state your roblox username.");
+            questionsArray.add("please state the roblox username of the suspected exploiter");
+            questionsArray.add("please describe what happened");
+            askQuestion();
+        }
+
+        public boolean askQuestion(){
+            if(currentIndex <= questionsArray.size() - 1){
+                user.openPrivateChannel().complete().sendMessage(questionsArray.get(currentIndex)).queue();
+                currentIndex += 1;
+                return false;
+            } else {
+                user.openPrivateChannel().complete().sendMessage("Thanks for reporting the exploit! Our moderation teams will look over the report you sent and investigate into the suspected exploiter."
+                + " To protect the privacy of you and the exploiter, the exploit report will not be publicly visible").queue();
+                createReport();
+                jda.removeEventListener(this);
+                Main.BugReports += 1;
+                hasFinished = true;
+                return true;
+            }
+        }
+
+        @Override
+        public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
+            if(event.getAuthor().equals(user)){
+                System.out.println(event.getMessage().getContentRaw());
+                questionAnswers.add(event.getMessage().getContentRaw());
+                askQuestion();
+                hasResponded = true;
+            }
+        }
+
+        public void createReport(){
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setTitle("Exploit Report #" + Main.ExploitReports);
+            embedBuilder.addField("Reporter Roblox Username", questionAnswers.get(0), false);
+            embedBuilder.addField("Exploiter Roblox Username", questionAnswers.get(1), false);
+            embedBuilder.addField("Exploit Description", questionAnswers.get(2), false);
+            embedBuilder.setAuthor(user.getAsTag(), null, user.getAvatarUrl());
+            embedBuilder.setColor(Color.RED);
+            if(embedBuilder.isValidLength()){
+                guild.getTextChannelById("771192681899753483").sendMessage(embedBuilder.build()).queue();
             }
         }
 
