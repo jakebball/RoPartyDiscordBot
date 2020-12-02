a28f4a45cd6956e4462627bf2720245f320a29c4
jake <jakebball2014@gmail.com>
2020-12-01 16:36:25 -0800

    fixed token security

--- src/main/java/com/discord/main/Config.java
+++ src/main/java/com/discord/main/Config.java
@@ -1,10 +1,10 @@
 package com.discord.main;
 
 public class Config {
-    public static String token = "NzcwNDE4OTcyNDczNTU3MDU0.X5dSlA.F-_5cCrAD_p9A6b1fIuuGaorcl0";
+    public static String token;
     public static String prefix = "!";
 
     public Config() {
-        //this.token = System.getenv().get("token");
+        this.token = System.getenv().get("token");
     }
 }
